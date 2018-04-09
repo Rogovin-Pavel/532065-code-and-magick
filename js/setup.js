ebb0b5fa392d5e737ce317b59c911fa3854eddb9
Pavel Rogovin <rogovin.pavel@gmail.com>
2018-04-09 14:24:03 +0300

    добавили функцию getRandomElement

--- js/setup.js
+++ js/setup.js
@@ -15,13 +15,28 @@ var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)'
 var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
 var MIN_ARRAY_LENGTH = 0;
 
+/**
+ * Нахождение случайного числа в диапазоне
+ * @param {number} min минимальное число в диапазоне
+ * @param {number} max максимальное число в диапазоне
+ * @return {number} возвращаем случайное число от min до max
+ */
 var randomInteger = function (min, max) {
   return Math.round(min - 0.5 + Math.random() * (max - min + 1));
 };
 
+/**
+ * Нахождение случайного элемента в массиве
+ * @param {Array} array передаем массив данных
+ * @return {*} возвращаем случайный элемент массива
+ */
+var getRandomElement = function (array) {
+  return array[randomInteger(MIN_ARRAY_LENGTH, array.length - 1)];
+};
+
 for (var i = 0; i < 4; i++) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
-  wizardElement.querySelector('.setup-similar-label').textContent = WIZARD_NAMES[randomInteger(MIN_ARRAY_LENGTH, WIZARD_NAMES.length - 1)] + ' ' + WIZARD_SURNAMES[randomInteger(MIN_ARRAY_LENGTH, WIZARD_SURNAMES.length - 1)];
+  wizardElement.querySelector('.setup-similar-label').textContent = getRandomElement(WIZARD_NAMES) + ' ' + WIZARD_SURNAMES[randomInteger(MIN_ARRAY_LENGTH, WIZARD_SURNAMES.length - 1)];
   wizardElement.querySelector('.wizard-coat').style.fill = coatColor[randomInteger(MIN_ARRAY_LENGTH, coatColor.length - 1)];
   wizardElement.querySelector('.wizard-eyes').style.fill = eyesColor[randomInteger(MIN_ARRAY_LENGTH, eyesColor.length - 1)];
   similarListElement.appendChild(wizardElement);
